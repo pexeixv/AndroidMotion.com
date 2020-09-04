2d77c1b8ab94bda63d085a76bb0d780461129561
Gavin Pereira <gavineditz@gmail.com>
2020-09-04 18:34:39 +0530

    9

--- script.js
+++ script.js
@@ -6,3 +6,19 @@ $(window).on("scroll", function() {
        $(".header").removeClass("active");
     }
 });
+$(window).on("scroll", function() {
+    if($(window).scrollTop() > 50) {
+        $(".icon").addClass("iconActive");
+    } else {
+        //remove the background property so it comes transparent again (defined in your css)
+       $(".icon").removeClass("iconActive");
+    }
+});
+$(window).on("scroll", function() {
+    if($(window).scrollTop() > 50) {
+        $(".aniHead").addClass("aniHeadAt");
+    } else {
+        //remove the background property so it comes transparent again (defined in your css)
+       $(".aniHead").removeClass("aniHeadAt");
+    }
+});
